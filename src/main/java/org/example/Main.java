e8340cb9f3d82e7300d0a7489bcd99917c386754
rami-abdalla <rami.abdallaa@gmail.com>
2024-11-06 19:50:57 -0500

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-
+        System.out.println("Hello world!");
+        for (int i=0;i<100;i++){
+            System.out.println(i);
+        }
         }
     }
